1962ee300f4fc567ccaaee31acfe393465f58ec7
DanushAthithya <83062926+DanushAthithya@users.noreply.github.com>
2023-08-09 17:20:51 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import Navbar from './nav';
+import Sec1  from './Sec1';
+import Sec2 from './Sec2';
+import Sec3 from './Sec3';
+import Sec4 from './Sec4';
+import Sec5 from './Sec5';
+import PreLoad from './PreLoad';
+
 
 function App() {
+  const [load, updateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      updateLoad(false);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <PreLoad load={load}/>
+      <Sec1/>
+      <Sec2/>
+      <Sec3/>
+      <Sec4/>
+      <Sec5/>
     </div>
   );
 }
